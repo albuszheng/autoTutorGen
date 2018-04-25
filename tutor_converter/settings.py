d8f382cbe4e7f68efc612bb8493f5bae3b91bce5
Keyang Zheng <albuszheng@hotmail.com>
2018-04-25 15:07:46 -0400

    deployment 6

--- tutor_converter/settings.py
+++ tutor_converter/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/converter/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 # SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
