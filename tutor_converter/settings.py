31b390b02ae8fcf2e8c7d03e5a500a79ce4b5de6
Keyang Zheng <albuszheng@hotmail.com>
2018-04-25 14:37:13 -0400

    deployment

--- tutor_converter/settings.py
+++ tutor_converter/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import whitenoise
+import django_heroku
 
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +28,7 @@ SECRET_KEY = '+gu1dww^=fzy1afk8jof1befqr-h7-0r2t_0r-f@($+&#n=58d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.0.0.27", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["10.0.0.27", "localhost", "127.0.0.1", "tutor-converter.herokuapp.com"]
 
 
 # Application definition
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'tutor_converter.urls'
@@ -119,8 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
