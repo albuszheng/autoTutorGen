5ab713a084e59cb7542b6d5f72e4a7d3f21d281f
Keyang Zheng <albuszheng@hotmail.com>
2018-04-25 22:56:50 -0400

    change db

--- tutor_converter/settings.py
+++ tutor_converter/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import whitenoise
 import django_heroku
+import dj_database_url
 # from dj_static import Cling
 
 # django_heroku.settings(locals())
@@ -80,13 +81,14 @@ WSGI_APPLICATION = 'tutor_converter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
