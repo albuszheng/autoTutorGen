8aebc3bfd6fb62d9a8d70f0f5a21a7e35889a507
Keyang Zheng <albuszheng@hotmail.com>
2018-04-27 13:54:39 -0400

    adding list and dict and tuple

--- converter/brd/trace_analysis.py
+++ converter/brd/trace_analysis.py
@@ -46,7 +46,7 @@ def get_cognitive_model(variables: list, db_trace: dict):
     # initialization
     snapshot = {"line": 1, "previous_line": 1}
     for variables in variables:
-        snapshot[variables] = 0
+        snapshot[variables] = None
 
     cognitive_model.append({"changed": "line", "value": snapshot['line'], "line": snapshot['line']})
     # print(cognitive_model)
@@ -56,8 +56,16 @@ def get_cognitive_model(variables: list, db_trace: dict):
         has_error, msg = check_trace_result(step)
         if not has_error:
             g_vars = step['globals']
+            heaps = step['heap']
             for var_name in g_vars:
-                if g_vars[var_name] != snapshot[var_name]:
+                if type(g_vars[var_name]) is list:
+                    new_heap = heaps[str(g_vars[var_name][1])]
+                    if new_heap != snapshot[var_name]:
+                        difference = (heap_difference(snapshot[var_name], new_heap))
+                        for d in difference:
+                            cognitive_model.append({"changed": var_name, "value": d['changed_value'], "line": snapshot['line']})
+                    snapshot = update_snapshot(var_name, new_heap, snapshot)
+                elif g_vars[var_name] != snapshot[var_name]:
                     cognitive_model.append({"changed": var_name, "value": g_vars[var_name], "line": snapshot['line']})
                     # print({"changed": var_name, "value": g_vars[var_name], "line": snapshot['previous_line']})
                     snapshot = update_snapshot(var_name, g_vars[var_name], snapshot)
@@ -81,6 +89,58 @@ def get_cognitive_model(variables: list, db_trace: dict):
 
     return cognitive_model, variables
 
+
+def heap_difference(old_heap:list, new_heap:list):
+    print(new_heap[0])
+    heap_type = new_heap[0]
+    if heap_type == "LIST":
+        return heap_list(old_heap,new_heap)
+    elif heap_type == "DICT":
+        return heap_dict(old_heap, new_heap)
+    elif heap_type == "TUPLE":
+        return heap_tuple(old_heap, new_heap)
+
+def heap_dict(old_heap:list, new_heap:list):
+    old_len = len(old_heap) if old_heap is not None else 0
+    new_len = len(new_heap)
+    result = []
+    if old_len != new_len:
+        for i in range(0, new_len):
+            if i < old_len and old_heap[i] != new_heap[i]:
+                result.append({"changed_value": "[" + new_heap[i][0] + "]" + ":" + str(new_heap[i+1][1])})
+            else:
+                result.append({"changed_value": "[" + new_heap[i][0] + "]" + ":" + str(new_heap[i+1][1])})
+    else:
+        for i in range(0, old_len):
+            if old_heap[i] != new_heap[i]:
+                result.append({"changed_value": "[" + new_heap[i][0] + "]" + ":" + str(new_heap[i+1][1])})
+    return result
+
+def heap_list(old_heap:list, new_heap:list):
+    old_len = len(old_heap) if old_heap is not None else 0
+    new_len = len(new_heap)
+    result = []
+    if old_len != new_len:
+        for i in range(0, new_len):
+            if i < old_len and old_heap[i] != new_heap[i]:
+                result.append({"changed_value": "[" + str(i) + "]" + ":" + str(new_heap[i+1])})
+            else:
+                result.append({"changed_value": "[" + str(i) + "]" + ":" + str(new_heap[i+1])})
+    else:
+        for i in range(0, old_len):
+            if old_heap[i] != new_heap[i]:
+                result.append({"changed_value": "[" + str(i) + "]" + ":" + str(new_heap[i+1])})
+    return result
+
+def heap_tuple(old_heap:list, new_heap:list):
+    result = []
+    list_diff =  list(set(old_heap) - set(new_heap))
+    for item in list_diff:
+        if item in old_heap:
+            result.append({"changed_value": "del:" + str(item)})
+        elif item in new_heap:
+            result.append({"changed_value": "add:" + str(item)})
+    return result
 
 m,v = get_cognitive_model(['x', 'i'], files[0]['trace'])
 for temp in m:
